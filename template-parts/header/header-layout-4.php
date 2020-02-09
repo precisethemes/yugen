81eefad873c17625c65b2aada352aee1db0596cc
volaj <volaj.co@gmail.com>
2020-02-09 14:36:21 +0545

    Fixd accessibility keyboard navigation

--- template-parts/header/header-layout-4.php
+++ template-parts/header/header-layout-4.php
@@ -23,11 +23,11 @@
 
                 if ( is_front_page() && is_home() ) : ?>
 
-                    <h1 class="site-title"><a class="d-inline-block td-none outline-none" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $site_title ); ?></a></h1>
+                    <h1 class="site-title"><a class="d-inline-block td-none" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $site_title ); ?></a></h1>
 
                 <?php else : ?>
 
-                    <p class="site-title"><a class="d-inline-block td-none outline-none" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $site_title ); ?></a></p>
+                    <p class="site-title"><a class="d-inline-block td-none" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $site_title ); ?></a></p>
 
                 <?php endif;
 
